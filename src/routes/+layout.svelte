28868048efcefef6d96eb536c19bb87c2cc2ec92
greatbacon <hamiltonjreed@gmail.com>
2026-08-22 16:15:24 -0600

    set header to link home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
 	import { AppBar, AppLayout, NavItem } from 'svelte-ux';
 
 	let { data, children } = $props();
@@ -13,6 +14,9 @@
 		</svelte:fragment>
 
 		<AppBar title="Loom" class="bg-primary text-primary-content">
+			<div slot="title" class="ml-2 text-lg font-medium">
+				<a href={resolve('/')}>Loom</a>
+			</div>
 			<div slot="actions">
 				<a href="/auth/logout">Sign out</a>
 			</div>
