f8e4e122ab109acf216a07df7cf18c7d7bb98290
greatbacon <hamiltonjreed@gmail.com>
2026-08-21 22:21:05 -0600

    round it off

--- src/lib/system/invitations/invitationsRepo.ts
+++ src/lib/system/invitations/invitationsRepo.ts
@@ -1,5 +1,5 @@
 import { Repo } from '$lib/db/repo';
-import type { Invitation } from '$lib/system/invitations/invitationsService';
+import type { Invitation, InvitationId } from '$lib/system/invitations/invitationsService';
 
 //TODO replace with a proper logger system
 const log = console;
@@ -25,5 +25,32 @@ export class InvitationRepo extends Repo {
 		`;
 		log.debug('[findAllByCreator] result', data);
 		return data;
+	}
+
+	async updateExpiration(
+		invite_id: InvitationId,
+		created_by: string,
+		expires_at: Date
+	): Promise<Invitation | undefined> {
+		log.debug(`[updateExpiration] invitation ${invite_id} for ${created_by}`);
+		const data = await this.sql<Invitation[]>`
+			UPDATE invitations
+			SET expires_at = ${expires_at}, updated_at = now()
+			WHERE invite_id = ${invite_id} AND created_by = ${created_by}
+			RETURNING invite_id, invite_code, created_by, used_by, expires_at, created_at, updated_at
+		`;
+		log.debug('[updateExpiration] result', data);
+		return data[0];
+	}
+
+	async delete(invite_id: InvitationId, created_by: string): Promise<Invitation | undefined> {
+		log.debug(`[delete] invitation ${invite_id} for ${created_by}`);
+		const data = await this.sql<Invitation[]>`
+			DELETE FROM invitations
+			WHERE invite_id = ${invite_id} AND created_by = ${created_by}
+			RETURNING invite_id, invite_code, created_by, used_by, expires_at, created_at, updated_at
+		`;
+		log.debug('[delete] result', data);
+		return data[0];
 	}
 }
